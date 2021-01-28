152add63b798f1f3c28c1b07cd60e6d8b89c9124
milakirn <47572461+milakirn@users.noreply.github.com>
2021-01-28 14:14:18 +0100

    Inition Commit

--- Lista_zaliczeniowa/zadanie1.py
+++ Lista_zaliczeniowa/zadanie1.py
@@ -9,4 +9,13 @@ def obliczanie_netto(brutto):
     prawie_calosc =  brutto - part
     zus = skladka_podstawowa * 0.0775
     zaliczka = prawie_calosc * 0.17 - 43.76 - zus  #kwota 43.76 jest ustalona ustawowo
+    if zaliczka < 0:
+        zaliczka = 0
+    return round((brutto - (zaliczka + part)), 2)
 
+if __name__ == "__main__":
+
+    kwota = float(input("Podaj swój zarobek brutto: "))
+    print(f"Twój zarobek netto wynosi {obliczanie_netto(kwota)} złotych")
+    kradziez = kwota - obliczanie_netto(kwota)
+    print(f"Państwo zabiera u Ciebie {kradziez} złotych!")
